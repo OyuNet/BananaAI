80b8a7d4d1d9cdcf1f5343cd79b844498699ddd3
OyuNet <oyunarda662@gmail.com>
2024-08-24 00:49:48 +0300

    early stopping

--- train_model.py
+++ train_model.py
@@ -21,11 +21,18 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
+early_stopping = EarlyStopping(
+    monitor='val_loss',  # İzlenecek metrik (doğrulama kaybı)
+    patience=3,          # Eğitim, doğrulama kaybı iyileşmediğinde kaç epoch bekleyecek
+    restore_best_weights=True  # En iyi ağırlıklar geri yüklenecek
+)
+
 epochs = 20
 history = model.fit(
     train_dataset,
     validation_data=validation_dataset,
     epochs=epochs,
+    callbacks=[early_stopping]
 )
 
 model.save('fruit_classifer.keras')
